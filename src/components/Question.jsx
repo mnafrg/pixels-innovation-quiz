bad909dbee44e8ce8e4d477ef86715e0e62b155d
MENNA AHMED <Menna_20220511@fci.helwan.edu.eg>
2026-02-10 04:18:12 +0200

    Update Question.jsx

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,12 +1,21 @@
-export default function Question({ data, onAnswer }) {
-    return (
-        <div>
-            <h2>{data.question}</h2>
-            {data.options.map((opt, i) => (
-                <button key={i} onClick={() => onAnswer(opt.scores)}>
-                    {opt.text}
-                </button>
-            ))}
-        </div>
-    );
+export default function Question({ data, onAnswer, progress }) {
+  return (
+    <div className="card">
+      <div className="progress-bar">
+        <div className="progress" style={{ width: `${progress}%` }}></div>
+      </div>
+
+      <h2>{data.question}</h2>
+
+      {data.options.map((opt, i) => (
+        <button
+          key={i}
+          className="option-btn"
+          onClick={() => onAnswer(opt.scores)}
+        >
+          {opt.text}
+        </button>
+      ))}
+    </div>
+  );
 }
