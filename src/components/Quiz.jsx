9913daba50d147e706581c9ab5c9cb433871f0d0
MENNA AHMED <Menna_20220511@fci.helwan.edu.eg>
2026-02-11 00:10:07 +0200

    Update Quiz.jsx

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,37 +4,36 @@ import Question from "./Question";
 import Result from "./Result";
 
 export default function Quiz() {
+  const [currentQ, setCurrentQ] = useState(0);
   const [scores, setScores] = useState({
-  hr: 0,
-  multimedia: 0,
-  academic: 0,
-  pr: 0
-});
+    hr: 0,
+    multimedia: 0,
+    academic: 0,
+    pr: 0
+  });
+  const [showResult, setShowResult] = useState(false);
 
-    const [showResult, setShowResult] = useState(false);
+  const handleAnswer = (optionScores) => {
+    const updated = { ...scores };
+    for (let key in optionScores) {
+      updated[key] += optionScores[key];
+    }
+    setScores(updated);
 
-    const handleAnswer = (optionScores) => {
-        const updated = { ...scores };
-        for (let key in optionScores) {
-            updated[key] += optionScores[key];
-        }
-        setScores(updated);
+    if (currentQ + 1 < questions.length) {
+      setCurrentQ(currentQ + 1);
+    } else {
+      setShowResult(true);
+    }
+  };
 
-        if (currentQ + 1 < questions.length) {
-            setCurrentQ(currentQ + 1);
-        } else {
-            setShowResult(true);
-        }
-    };
-
-   return showResult ? (
-  <Result scores={scores} />
-) : (
-  <Question
-    data={questions[currentQ]}
-    onAnswer={handleAnswer}
-    progress={((currentQ) / questions.length) * 100}
-  />
-);
-
+  return showResult ? (
+    <Result scores={scores} />
+  ) : (
+    <Question
+      data={questions[currentQ]}
+      onAnswer={handleAnswer}
+      progress={(currentQ / questions.length) * 100}
+    />
+  );
 }
